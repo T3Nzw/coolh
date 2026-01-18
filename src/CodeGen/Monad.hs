03d0982fe322c6350da7ff392b5c6e250e2eb9fc
T3Nzw <t3nszw@gmail.com>
2026-01-18 20:38:51 +0200

    oops

--- src/CodeGen/Monad.hs
+++ src/CodeGen/Monad.hs
@@ -39,20 +39,11 @@ addi reg1 reg2 imm = addInstr $ Addi reg1 reg2 imm
 sub :: Reg -> Reg -> Reg -> CodeGen ()
 sub reg1 reg2 reg3 = addInstr $ Sub reg1 reg2 reg3
 
-subi :: Reg -> Reg -> Imm -> CodeGen ()
-subi reg1 reg2 imm = addInstr $ Subi reg1 reg2 imm
-
 mul :: Reg -> Reg -> Reg -> CodeGen ()
 mul reg1 reg2 reg3 = addInstr $ Mul reg1 reg2 reg3
 
-muli :: Reg -> Reg -> Imm -> CodeGen ()
-muli reg1 reg2 imm = addInstr $ Muli reg1 reg2 imm
-
 div :: Reg -> Reg -> Reg -> CodeGen ()
 div reg1 reg2 reg3 = addInstr $ Div reg1 reg2 reg3
-
-divi :: Reg -> Reg -> Imm -> CodeGen ()
-divi reg1 reg2 imm = addInstr $ Divi reg1 reg2 imm
 
 beq :: Reg -> Reg -> Label -> CodeGen ()
 beq reg1 reg2 lbl = addInstr $ Beq reg1 reg2 lbl
